1cf709a6bdad37279baa0028c1034f438e87b5b2
Lee <dlckdwns0927@gmail.com>
2023-01-31 16:41:52 +0900

    ExtractedView 사용

--- Day 2/Flag/Flag/ContentView.swift
+++ Day 2/Flag/Flag/ContentView.swift
@@ -13,20 +13,7 @@ struct ContentView: View {
             ForEach(Region.all, id: \.title) { region in
                 Section(region.title) {
                     ForEach(region.countries, id: \.name) { country in
-                        HStack {
-                            Image(country.file)
-                                .resizable()
-                                .frame(width: 60,height: 60)
-                            VStack {
-                                Text(country.name)
-                                    .font(.title)
-                                    .frame(maxWidth: .infinity,alignment: .leading)
-                                Text("\(country.name.count) million people")
-                                    .frame(maxWidth: .infinity,alignment: .leading)
-                                    .foregroundColor(.gray)
-                                    .shadow(radius: 9)
-                            }
-                        }
+                        CountryView(country: country) //생성자 넘겨주기
                     }
                 }
             }
@@ -36,6 +23,27 @@ struct ContentView: View {
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
+        }
+    }
+}
+
+struct CountryView: View { //command + click , ExtractedView , rename
+    let country : Country
+    
+    var body: some View {
+        HStack {
+            Image(country.file)
+                .resizable()
+                .frame(width: 60,height: 60)
+            VStack {
+                Text(country.name)
+                    .font(.title)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                Text("\(country.name.count) million people")
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                    .foregroundColor(.gray)
+                    .shadow(radius: 9)
+            }
         }
     }
 }
