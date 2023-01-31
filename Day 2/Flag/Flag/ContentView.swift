d04a305e8e968aa83cbc00316c605b955fd2060b
Lee <dlckdwns0927@gmail.com>
2023-01-31 16:35:07 +0900

    Flag App

---
+++ Day 2/Flag/Flag/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Flag
+//
+//  Created by 홍길동 on 2023/01/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            ForEach(Region.all, id: \.title) { region in
+                Section(region.title) {
+                    ForEach(region.countries, id: \.name) { country in
+                        HStack {
+                            Image(country.file)
+                                .resizable()
+                                .frame(width: 60,height: 60)
+                            VStack {
+                                Text(country.name)
+                                    .font(.title)
+                                    .frame(maxWidth: .infinity,alignment: .leading)
+                                Text("\(country.name.count) million people")
+                                    .frame(maxWidth: .infinity,alignment: .leading)
+                                    .foregroundColor(.gray)
+                                    .shadow(radius: 9)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
---
+++ Day 2/Flag/Flag/FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagApp.swift
+//  Flag
+//
+//  Created by 홍길동 on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
