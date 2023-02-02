cd557386054d7b50f4bc0e8ca48c70bdd991f96a
Lee <dlckdwns0927@gmail.com>
2023-02-02 15:35:59 +0900

    M_G
    game score add

--- Day 4/MemoryGame/MemoryGame/GameModel.swift
+++ Day 4/MemoryGame/MemoryGame/GameModel.swift
@@ -21,28 +21,55 @@ struct Card {
 class GameModel :ObservableObject { //게임 진행의 모든것, class만 옵저버블이 될수이씀
     static let cols = 3 // 코드 수정은 여기서 바꾸면됌
     static let rows = 4 // 정적,상수 , class 멤버, GameModel.~~
+    var openCardIndex: Int? //열려있는 카드의 인덱스값이 없을수도있기때문에 옵셔널 사용
+    var score: Double = 0
+    
     
     @Published var cards = [Card]() //어느 멤버의 수정이 있을때 알림이 가게 하느냐 ? -> Published 
     //var cards : [Card] = [] 같은 표현 [~] = [] Card Array를 만들어서 cards에 넣겠다
     
     init() {
+        start()
+    }
+    
+    func start() {
+        score = 0
+        cards = []
         let max =  Self.cols * Self.rows / 2
         for number in 1...max {
-            cards.append(Card(number: number,state: .open))
+            cards.append(Card(number: number,state: .closed))
             cards.append(Card(number: number, state: .closed)) // 다른언어의 경우 reference type이여서 안되지만, swift는 value type이여서 둘은 다른거임
             
         }
+        openCardIndex = nil //이전 게임 데이터를 가지고 있을 수 있음
     }
+    
     func card(row: Int, col: Int) -> Card {
             let index = row * Self.cols + col //여기서 대문자 Self는 gameModle
             return cards[index]
         }
     func toggle(row: Int, col: Int) { // class는 mutating을 안써도됌, 항상 가능
         let index = row * Self.cols + col
-        var card = cards[index]
-        card.state = card.state == .open ? .closed : .open
-        cards[index] = card
+        
+        if index == openCardIndex {
+            return
+        }
+        // if-let 으로 지정된 상수는 그 값이 nil이 아닐 때에만 이하 코드가 실행
+        if let oci = openCardIndex {
+            if (cards[oci].number == cards[index].number){
+                cards[oci].state = .removed
+                cards[index].state = .removed
+                openCardIndex = nil
+                score += 10.0
+                return
+            }
+            cards[oci].state = .closed
+        }
+        cards[index].state = .open
+        openCardIndex = index
+        score -= 1.0
     }
+   
 }
 
 
--- Day 4/MemoryGame/MemoryGame/GameView.swift
+++ Day 4/MemoryGame/MemoryGame/GameView.swift
@@ -8,17 +8,53 @@
 import SwiftUI
 
 struct GameView: View {
+    @State var showsRetryAlert = false
+    
+    //s를 붙이는 것은 부울값을 정할때
     @ObservedObject var gameModel = GameModel() // gameview가 생성 -> modelview 생성
     //내가 멤버변수로 가지고있는 것의 멤버변수를 바꿀때, 게임모델의 변화가 있을때 다시 그리겠다
     var body: some View {
         VStack {
+            HStack {
+                Text("Memory Game")
+                    .font(.largeTitle)
+                Spacer()
+                Text("Score: \(String(format: "%.1f     ", gameModel.score))")
+                    .font(.headline)
+            }
+            
             GrideStackView(cols: GameModel.cols, rows: GameModel.rows) { row, col in
                 CardView(prefix: "f", card: gameModel.card(row : row, col : col))
                     .onTapGesture {
                         gameModel.toggle(row: row, col: col)
                     }
             }
+            Spacer()
+            HStack {
+                Button {
+                    //gameModel.start()
+                    showsRetryAlert = true
+                } label: {
+                     Text("Restart")
+                        .font(.largeTitle)
+                        .padding()
+                        .background(Capsule()
+                            .stroke(lineWidth: 3))
+                        .shadow(radius: 3)
+                }
+                
+            }
+            Spacer()
         }
+        .alert(isPresented: $showsRetryAlert) {
+                    Alert(title: Text("Restart"),
+                          message: Text("Do you really want to restart the game?"),
+                          primaryButton:
+                            .destructive(Text("Restart"))  {
+                                gameModel.start()
+                            },
+                          secondaryButton: .cancel())
+                }
     }
 }
 
