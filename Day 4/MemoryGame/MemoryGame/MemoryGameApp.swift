0845c7ecc68a99f375a598c7d0b64cd7dd7bb933
Lee <dlckdwns0927@gmail.com>
2023-02-02 10:39:34 +0900

    MemoryGame
    start

---
+++ Day 4/MemoryGame/MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by 홍길동 on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
