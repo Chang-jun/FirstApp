203d333be90103bd7728012f2db7b493783d3098
Lee <dlckdwns0927@gmail.com>
2023-02-02 14:26:16 +0900

    M_G
    카드 뒤집기 기능(Observed object,publish)

--- Day 4/MemoryGame/MemoryGame/GameModel.swift
+++ Day 4/MemoryGame/MemoryGame/GameModel.swift
@@ -18,26 +18,31 @@ struct Card {
     //삭제 되는 경우 ture -> open, false -> back , nil -> delete?
 }
 
-struct GameModel { //게임 진행의 모든것
+class GameModel :ObservableObject { //게임 진행의 모든것, class만 옵저버블이 될수이씀
     static let cols = 3 // 코드 수정은 여기서 바꾸면됌
     static let rows = 4 // 정적,상수 , class 멤버, GameModel.~~
     
-    var cards = [Card]()
+    @Published var cards = [Card]() //어느 멤버의 수정이 있을때 알림이 가게 하느냐 ? -> Published 
     //var cards : [Card] = [] 같은 표현 [~] = [] Card Array를 만들어서 cards에 넣겠다
     
     init() {
         let max =  Self.cols * Self.rows / 2
         for number in 1...max {
-            let max = Card(number: number)
             cards.append(Card(number: number,state: .open))
             cards.append(Card(number: number, state: .closed)) // 다른언어의 경우 reference type이여서 안되지만, swift는 value type이여서 둘은 다른거임
             
         }
     }
     func card(row: Int, col: Int) -> Card {
-            let index = row * Self.cols + col
+            let index = row * Self.cols + col //여기서 대문자 Self는 gameModle
             return cards[index]
         }
+    func toggle(row: Int, col: Int) { // class는 mutating을 안써도됌, 항상 가능
+        let index = row * Self.cols + col
+        var card = cards[index]
+        card.state = card.state == .open ? .closed : .open
+        cards[index] = card
+    }
 }
 
 
--- Day 4/MemoryGame/MemoryGame/GameView.swift
+++ Day 4/MemoryGame/MemoryGame/GameView.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct GameView: View {
-    var gameModel = GameModel() // gameview가 생성 -> modelview 생성
+    @ObservedObject var gameModel = GameModel() // gameview가 생성 -> modelview 생성
+    //내가 멤버변수로 가지고있는 것의 멤버변수를 바꿀때, 게임모델의 변화가 있을때 다시 그리겠다
     var body: some View {
         VStack {
             GrideStackView(cols: GameModel.cols, rows: GameModel.rows) { row, col in
                 CardView(prefix: "f", card: gameModel.card(row : row, col : col))
+                    .onTapGesture {
+                        gameModel.toggle(row: row, col: col)
+                    }
             }
         }
     }
