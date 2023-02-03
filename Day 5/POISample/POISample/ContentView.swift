e0aaadb155544d4a0555b9846cc7da1d8549e0b2
Lee <dlckdwns0927@gmail.com>
2023-02-03 16:20:31 +0900

    Poi sample
    지도, 전화버튼

--- Day 5/POISample/POISample/ContentView.swift
+++ Day 5/POISample/POISample/ContentView.swift
@@ -4,24 +4,54 @@
 //
 //  Created by 홍길동 on 2023/02/03.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var poiData = PoiDataStore.get()
     var body: some View {
-        VStack {
-            
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(poiData.items, id: \.RESTRT_NM) { item in
+                        PoiItemView(item: item)
+                    }
+                }
+            }
+            .navigationTitle("Restaurants")
         }
         .onAppear {
             poiData.startLoading()
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct PoiItemView: View {
+    let item: PoiItem
+    var body: some View {
+        NavigationLink {
+            Text(item.RESTRT_NM)
+        } label: {
+            HStack {
+                Image(systemName: "fork.knife.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 64)
+                VStack(alignment: .leading) {
+                    Text(item.RESTRT_NM)
+                        .font(.title)
+                        .lineLimit(2)
+                        .foregroundColor(.blue)
+                    Text(item.REPRSNT_FOOD_NM)
+                        .font(.body)
+                        .foregroundColor(.gray)
+                }
+            }
+        }
     }
 }
--- Day 5/POISample/POISample/PoiData.swift
+++ Day 5/POISample/POISample/PoiData.swift
@@ -7,9 +7,7 @@
 
 import Foundation
 
-
 class PoiItem: Codable {
-    
     let SIGUN_NM: String
     let SIGUN_CD: String
     let RESTRT_NM: String
@@ -25,11 +23,12 @@ class PoiItem: Codable {
 class PoiDataStore: ObservableObject {
     private static let singleton = PoiDataStore()
     public static func get() -> PoiDataStore { return singleton }
+
     struct Const {
         static let baseUrl = "https://openapi.gg.go.kr/PlaceThatDoATasteyFoodSt"
         static let key = "e92ec3e6a9f44981974d4746d28f8995"
     }
-
+    
     @Published var items = [PoiItem]()
 
     func startLoading() {
@@ -55,38 +54,38 @@ class PoiDataStore: ObservableObject {
         task.resume()
     }
     func parse(data: Data) -> [PoiItem]? {
-            guard let root = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
-                return nil
-            } //json객체 생성
-            guard let arr = root["PlaceThatDoATasteyFoodSt"] as? [Any] else {
-                return nil
-            }
-            guard let rowobj = arr[1] as? [String: Any] else {
-                return nil
-            }
-            guard let items = rowobj["row"] as? [Any] else {
-                return nil
-            }
-            var pois = [PoiItem]()
-            for item in items {
-                guard let poi = PoiItem.from(dictionary: item) else {
-                    continue
-                }
-                pois.append(poi)
-            }
-            return pois
+        guard let root = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+            return nil
         }
+        guard let arr = root["PlaceThatDoATasteyFoodSt"] as? [Any] else {
+            return nil
+        }
+        guard let rowobj = arr[1] as? [String: Any] else {
+            return nil
+        }
+        guard let items = rowobj["row"] as? [Any] else {
+            return nil
+        }
+        var pois = [PoiItem]()
+        for item in items {
+            guard let poi = PoiItem.from(dictionary: item) else {
+                continue
+            }
+            pois.append(poi)
+        }
+        return pois
     }
+}
 
-    fileprivate extension PoiItem {
-        static func from(dictionary: Any) -> PoiItem? {
-            guard let data = try? JSONSerialization.data(withJSONObject: dictionary) else {
-                return nil
-            }
-            let decoder = JSONDecoder()
-            guard let item = try? decoder.decode(PoiItem.self, from: data) else {
-                return nil
-            }
-            return item
+fileprivate extension PoiItem {
+    static func from(dictionary: Any) -> PoiItem? {
+        guard let data = try? JSONSerialization.data(withJSONObject: dictionary) else {
+            return nil
         }
+        let decoder = JSONDecoder()
+        guard let item = try? decoder.decode(PoiItem.self, from: data) else {
+            return nil
+        }
+        return item
     }
+}
