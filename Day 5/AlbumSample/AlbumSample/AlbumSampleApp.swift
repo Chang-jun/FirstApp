9a4f020711f4f6fe50429c146267e92bed84736a
Lee <dlckdwns0927@gmail.com>
2023-02-03 10:52:10 +0900

    AlbumSample
    프로젝트 시작

---
+++ Day 5/AlbumSample/AlbumSample/AlbumSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumSampleApp.swift
+//  AlbumSample
+//
+//  Created by 홍길동 on 2023/02/03.
+//
+
+import SwiftUI
+
+@main
+struct AlbumSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AlbumSample()
+        }
+    }
+}
