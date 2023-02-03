30bf45b9d0fe567949d02dc05e58dae644026534
Lee <dlckdwns0927@gmail.com>
2023-02-03 12:03:30 +0900

    A-S
    AlbumItemView

--- Day 5/AlbumSample/AlbumSample/AlbumsStore.swift
+++ Day 5/AlbumSample/AlbumSample/AlbumsStore.swift
@@ -7,11 +7,13 @@
 
 import Foundation
 
-struct Album :Decodable { //Decodable을 명시해주면 객체 생성시 변수를 안만들어도 됌
-    let artistname : String
-    let albumTitle : String
-    let image : String
+
+struct Album: Decodable {
+    let artistName: String
+    let albumTitle: String
+    let image: String
 }
+
 
 struct AlbumResponse: Decodable {
     let albums: [Album]
@@ -23,28 +25,25 @@ class AlbumStore: ObservableObject {
     @Published var albums = [Album]()
     
     func load() {
-        guard let url = Bundle.main.url(forResource: "res/albums.json", withExtension: "")  else {
-        //url 의 타입은 옵셔널     //Bundle -> 이앱 자체
-            print("Bug????")
+        guard let url = Bundle.main.url(forResource: "res/albums.json", withExtension: "") else {
+            print("BUG??????")
             return
         }
+//        let url = Bundle.main.url(forResource: "res/albums.json", withExtension: "")!
         guard let data = try? Data(contentsOf: url) else {
             print("Data load failed")
             return
         }
-        //nil 일수도 있기때문에 가드 렛
         
         let decoder = JSONDecoder()
-        guard let albumResponse = try? decoder.decode(AlbumResponse.self, from : data) else {
-            print("Invalid Json")
+        guard let albumResponse = try? decoder.decode(AlbumResponse.self, from: data) else {
+            print("Invalid JSON?")
             return
         }
-        
+
         albums = albumResponse.albums
-        
     }
-    
-    
+
     
 }
 //객체가 하나만 존재해도 되는애 싱글턴,인스턴스
